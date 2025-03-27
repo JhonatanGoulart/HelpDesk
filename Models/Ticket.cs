42fb463fc2797b98b30989c7a73176c8c2d2d2ac
JhonatanGoulart <jhonatan.noc.fibinet@gmail.com>
2025-03-27 19:30:46 -0300

    Organização apenas

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -3,7 +3,7 @@ namespace HelpDeskPro.API.Models
     public class Ticket
     {
         public int Id { get; set; }                     // Identificador único do chamado
-        public string Titulo { get; set; }              // Título do problema/solicitação
+        public string Titulo { get; set; }              // Título do problema
         public string Descricao { get; set; }           // Detalhes do chamado
         public DateTime DataAbertura { get; set; }      // Data de abertura do chamado
         public DateTime? DataFechamento { get; set; }   // Data de fechamento (opcional)
--- Models/Users.cs
+++ Models/Users.cs
@@ -5,7 +5,7 @@ namespace HelpDeskPro.API.Models
         public int Id { get; set; }                 // Identificador único do usuário
         public string Nome { get; set; }            // Nome do usuário
         public string Email { get; set; }           // Email para contato
-        public string Cargo { get; set; }           // Cargo na empresa (ex: Técnico, Gerente)
-        public bool Ativo { get; set; }             // Indicador de conta ativa/inativa
+        public string Cargo { get; set; }           // Cargo na empresa 
+        public bool Ativo { get; set; }             // Indicador de conta ativa
     }
 }
